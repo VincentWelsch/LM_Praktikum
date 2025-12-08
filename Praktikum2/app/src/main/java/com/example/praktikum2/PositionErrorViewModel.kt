b62e8bdb300a62f32fe6f329721cccc327441e74
Vincent Welsch <welschvincent@gmail.com>
2025-12-08 21:45:55 +0100

    Added composable AnalyzeWindow and expanded PositionErrorViewModel.

--- Praktikum2/app/src/main/java/com/example/praktikum2/PositionErrorViewModel.kt
+++ Praktikum2/app/src/main/java/com/example/praktikum2/PositionErrorViewModel.kt
@@ -1,14 +1,60 @@
 package com.example.praktikum2
 
 import androidx.lifecycle.ViewModel
+import kotlin.math.pow
+import kotlin.math.sqrt
 
 class PositionErrorViewModel(private val collectionModel: CollectionViewModel): ViewModel() {
+    private var positionErrors: MutableList<Float> = mutableListOf()
+    fun getPositionErrors(): List<Float> { return positionErrors }
+
     fun calculatePositionErrors() {
-        // TODO
+        if (collectionModel.getGroundTruth().size != collectionModel.getWaypoints().size)
+            throw Throwable("Error: Ground truth and waypoints must be the same size")
+        positionErrors.clear()
+
+        val groundTruth = collectionModel.getGroundTruth() // Use for actual position of waypoints
+        val measurements = collectionModel.getMeasurements() // Use for position and time
+        val waypoints = collectionModel.getWaypoints() // Use for time of arrival at waypoints
+
+        for (measurement in collectionModel.getMeasurements()) {
+            for (i in 0 until collectionModel.getGroundTruth().size) {
+                // i is the index of the waypoint
+                // Loop through waypoints to find out which "Stütz-" and "Richtungsvektor" to use
+                if (measurement.time > collectionModel.getWaypoints()[i].time) {
+                    /* If tine of measurement is greater than time of waypoint for the first time,
+                    *  measurement must be between that waypoint and the next.
+                    *
+                    *  Interpolate ground truth position between waypoint and next waypoint using
+                    *  vectors. Let latitude be x and longitude be y
+                    *
+                    *  Let g be groundTruth and t_d be the time diff between waypoint and measurement.
+                    *  To get gtPosition:
+                    *  g[i] + t_d * (g[i + 1] - g[i])       both for x and for y
+                    *
+                    *  Error as difference between measurement and gtPosition:
+                    *  m - gtPosition                       both for x and for y
+                    */
+                    val timeDiff = measurement.time - waypoints[i].time
+                    val gtPosition = floatArrayOf(
+                        // Waypoint i as Stützvektor and "Verbindungsvektor" to next as "Richtungsvektor"
+                        groundTruth[i][0] + timeDiff * (groundTruth[i + 1][0] - groundTruth[i][0]),
+                        groundTruth[i][1] + timeDiff * (groundTruth[i + 1][1] - groundTruth[i][1])
+                    )
+                    val positionDiff = floatArrayOf(
+                        measurement.latitude - gtPosition[0],
+                        measurement.longitude - gtPosition[1]
+                    )
+                    // Calculate and add length of vector
+                    positionErrors.add(sqrt(positionDiff[0].pow( 2) + positionDiff[1].pow(2)))
+                }
+            }
+        }
+        positionErrors = mutableListOf()
     }
 
     // Get a CDF from a list of errors
-    fun positionErrorCDF(positionErrors: MutableList<Float>): List<FloatArray> {
+    fun positionErrorCDF(): List<FloatArray> {
         if (positionErrors.isEmpty())
             return emptyList()
         positionErrors.sort()
