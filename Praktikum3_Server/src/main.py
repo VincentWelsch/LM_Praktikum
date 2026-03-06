2952f52fe6ff29f1752b21e2c19d517fb64b2685
neubach-fx <209954794+neubach-fx@users.noreply.github.com>
2026-03-06 20:25:45 +0100

    Corrected the Endpoint for getting the fixes of a run

--- Praktikum3_Server/src/main.py
+++ Praktikum3_Server/src/main.py
@@ -86,38 +86,35 @@ async def report_fix(fixReport: FixReport):
 
 # endpoints further below are not really required by the tasks for Praktikum 3
 
-# currently doesnt work
 @app.get("/run/get/{runId}") # return all fixes of a run
 async def get_fixes(runId: str):
-    # get db connection
-    connection = connect_db()
-    if connection == None:
-        return {"message": "Connection failed", "success": 0, "result": {}}
+    conn = connect_db()
+    if not conn:
+        return {"success": 0, "message": "DB connection failed"}
     
-    answer = {"message": "Error", "success": 0, "result": {}}
-    with connection.cursor() as cursor: # open cursor
-        try:
-            # get fixes
-            cursor.execute("""SELECT longitude, latitude, altitude
-                           FROM fix
-                           WHERE \"runId\" = %s""", (runId,))
-            if not cursor.rowcount:
-                answer["message"] = "No run found"
-            else:
-                fixes = list()
-                rows = cursor.fetchall() # get rows with matching runId
-                for row in rows:
-                    # add fix with named fields using dict
-                    fix = {"longitude": row[0], "latitude": row[1], "altitude": row[2]}
-                    fixes.append(fix)
-                answer["result"] = fixes
-                answer["message"] = "Ok"
-        except psycopg2.Error as e:
-            print("SQL error:", e)
-            answer["message"] = "SQL error"
-        except Exception as e:
-            print("Error:", e)
-    connection.close()
+    # answer = {"success": 0, "message": "Error", "result": {}}
+    try:
+        cur = conn.cursor()
+        cur.execute("""SELECT longitude, latitude, altitude
+                        FROM fix
+                        WHERE runId = ?
+                    """,
+                    (runId,),
+        )
+        rows = cur.fetchall()
+        if not rows:
+            return {"success": 0, "message": f"No fixes for the ID {runId} found"}
+        else:
+            fixes = [
+                {"longitude": r[0], "latitude": r[1], "altitude": r[2]}
+                for r in rows
+            ]
+            return {"success": 1, "fixes": fixes}
+    except Exception as e:
+        print("Error:", e)
+        return {"success": 0, "message": "SQL error"}
+    finally:
+        conn.close()
     return answer
 
 @app.delete("/run/remove/{runId}") # remove a run and all its fixes by its runID
